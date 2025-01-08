619301e084a32366ac40e0664ff3e980e1aa2ca8
ynsdlbs <yunusdelibas@outlook.com>
2025-01-07 23:07:39 -0800

    title changed.

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def get_top_movies_by_genre(genre):
 
 # App Layout
 st.markdown(
-    "<h1 style='text-align: center;'>🎬📚 Book/Movie Recommendation App </h1>",
+    "<h1 style='text-align: center;'>🎬📚 Film Kitap Önersi </h1>",
     unsafe_allow_html=True
 )
 
